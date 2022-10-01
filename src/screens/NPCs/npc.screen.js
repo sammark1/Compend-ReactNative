172831efa4e51c17cb23380b54f07ba0a6eb1dae
Sammark1 <samaustinmark@gmail.com>
2022-10-01 15:00:28 -0400

    in-app data being rendered

--- src/screens/NPCs/npc.screen.js
+++ src/screens/NPCs/npc.screen.js
@@ -1,34 +1,81 @@
 import React from "react";
-import { Text, Image } from "react-native";
+import { Text, Image, FlatList } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { SafeView } from "../../infrastructure/util/safe-area.component";
 
-const NPCTable = styled.View`
+const NPCView = styled.View`
   flex: 1;
   background-color: gold;
 `;
 
 const NPCCard = styled(Card)`
-background-color: tomato;
-max-width: 200px;
-`
+  background-color: tomato;
+`;
+
+const NPCList = styled(FlatList)`
+  background-color: dodgerblue;
+`;
+
+const NPCArray = [
+  {
+    givenName: "Harry",
+    familyName: "Potter",
+    subrace: "",
+    race: "human",
+    class: "lv 20 wizard",
+  },
+  {
+    givenName: "Jeremy",
+    familyName: "Yu",
+    subrace: "",
+    race: "human",
+    class: "lv 9 programmer",
+  },
+  {
+    givenName: "Chugga",
+    familyName: "Conroy",
+    subrace: "drow",
+    race: "elf",
+    class: "lv 20 wizard",
+  },
+  {
+    givenName: "Geralt",
+    familyName: "Of Rivia",
+    subrace: "witcher",
+    race: "human",
+    class: "lv 17 Fighter",
+  },
+  {
+    givenName: "Ginny",
+    familyName: "Potter",
+    subrace: "",
+    race: "human",
+    class: "lv 18 wizard",
+  },
+];
 
 export const NPCs = () => {
   return (
     <SafeView>
-      <NPCTable>
-        <NPCCard mode="outlined">
-          <Card.Title title="NPC name" subtitle="NPC race and class" />
-        </NPCCard>
-        <Card>
-          <Card.Title title="NPC name" subtitle="NPC race and class" />
-        </Card>
-        <Card>
-          <Card.Title title="NPC name" subtitle="NPC race and class" />
-        </Card>
-      </NPCTable>
+      <NPCView>
+        {/* TODO space here for a header */}
+        <NPCList
+          data={NPCArray}
+          renderItem={({ item }) => {
+            return (
+              <NPCCard mode="">
+                <Card.Title
+                  title={item.givenName + " " + item.familyName}
+                  subtitle={`${item.subrace} ${item.race} ${item.class}`}
+                />
+              </NPCCard>
+            );
+          }}
+          keyExtractor={(item) => item.givenName+" "+item.familyName}
+        />
+      </NPCView>
     </SafeView>
   );
 };
