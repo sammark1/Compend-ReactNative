8c000153064a088f5f0ee108f38ef452f2fd207e
Sammark1 <samaustinmark@gmail.com>
2022-10-01 14:43:03 -0400

    in-progress basic NPC cards

--- src/screens/NPCs/npc.screen.js
+++ src/screens/NPCs/npc.screen.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
 
 import { SafeView } from "../../infrastructure/util/safe-area.component";
+
+const NPCTable = styled.View`
+  flex: 1;
+  background-color: gold;
+`;
+
+const NPCCard = styled(Card)`
+background-color: tomato;
+max-width: 200px;
+`
 
 export const NPCs = () => {
   return (
     <SafeView>
-      <Text>Hello Butt</Text>
+      <NPCTable>
+        <NPCCard mode="outlined">
+          <Card.Title title="NPC name" subtitle="NPC race and class" />
+        </NPCCard>
+        <Card>
+          <Card.Title title="NPC name" subtitle="NPC race and class" />
+        </Card>
+        <Card>
+          <Card.Title title="NPC name" subtitle="NPC race and class" />
+        </Card>
+      </NPCTable>
     </SafeView>
   );
 };
