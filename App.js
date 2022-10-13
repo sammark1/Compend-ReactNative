c8b7a7e8995a2a51ac4ec6453a3457674703923e
Sammark1 <samaustinmark@gmail.com>
2022-10-13 16:20:16 -0400

    moved campaignscontextprovider

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { ThemeProvider } from "styled-components/native";
 
 import { theme } from "./src/infrastructure/theme";
 import { SaveContextProvider } from "./src/services/save/save.context";
-import { CampaignsContextProvider } from "./src/services/campaigns/campaigns.context";
 import { AppNavigator } from "./src/infrastructure/navigation/app.navigator";
 
 export default function App() {
@@ -13,9 +12,9 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <SaveContextProvider>
-          <CampaignsContextProvider>
+          
             <AppNavigator />
-          </CampaignsContextProvider>
+          
         </SaveContextProvider>
       </ThemeProvider>
     </>
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -9,16 +9,23 @@ import { CampaignsList } from "../../screens/Campaigns/campaigns-list.screen";
 import { NPCs } from "../../screens/NPCs/npcs-list.screen";
 import { Admin } from "../../screens/Admin/admin.screen";
 import { CampaignDetail } from "../../screens/Campaigns/campaign-detail.screen";
+import { CampaignsContextProvider } from "./src/services/campaigns/campaigns.context";
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Campaigns" component={CampaignsList} />
-        <Stack.Screen name="Campaign Nav" component={CampaignNavigator} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CampaignsContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Campaigns" component={CampaignsList} />
+          <Stack.Screen
+            name="Campaign Nav"
+            component={CampaignNavigator}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CampaignsContextProvider>
   );
 };
