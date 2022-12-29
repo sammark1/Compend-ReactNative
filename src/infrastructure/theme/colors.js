f1adf91833953722680e33ce05194cccbfe1f44f
Sammark1 <samaustinmark@gmail.com>
2022-12-29 13:00:15 -0500

    npc list button styling

--- src/infrastructure/theme/colors.js
+++ src/infrastructure/theme/colors.js
@@ -2,12 +2,14 @@ export const colors = {
   background: {
     primary: "#DEC3A6",
     dark: "#D2AC84",
+    secondary: "#8C1500",
   },
   accent: {
     primary: "#8C1500",
   },
-  text:{
-    primary:"#8C1500",
+  text: {
+    primary: "#8C1500",
+    secondary: "#DEC3A6",
   },
   test: "#00ff00",
 };
--- src/screens/NPCs/npcs-list.screen.js
+++ src/screens/NPCs/npcs-list.screen.js
@@ -7,26 +7,36 @@ import { SafeView } from "../../infrastructure/util/safe-area.component";
 import { SaveContext } from "../../services/save/save.context";
 import { CampaignsContext } from "../../services/campaigns/campaigns.context";
 
-const TempButtonContainer = styled.View`
+const ButtonsContainer = styled.View`
   width: 100%;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
 `;
 
-const TempButton = styled.Button``;
+const HeaderButton = styled.Pressable`
+  margin: ${({ theme }) => theme.spacing.xs};
+  padding: ${({ theme }) => theme.spacing.sm};
+  background-color: ${({ theme }) => theme.colors.background.dark};
+  border-radius: ${({ theme }) => theme.spacing.xs};
+`;
+
+const ButtonText = styled.Text`
+  font-family: CormInSBold;
+  font-size:${({ theme }) => theme.sizes.point.md}
+  color:${({ theme }) => theme.colors.text.primary}
+`;
 
 const NPCView = styled.View`
   flex: 1;
 `;
 
 const NPCCard = styled(Card)`
-  background-color: ${({theme})=>theme.colors.background.dark};
+  background-color: ${({ theme }) => theme.colors.background.dark};
   margin-top: 2px;
 `;
 
-const NPCList = styled(FlatList)`
-`;
+const NPCList = styled(FlatList)``;
 
 export const NPCsList = ({ navigation }) => {
   const { campaign } = useContext(CampaignsContext);
@@ -35,14 +45,19 @@ export const NPCsList = ({ navigation }) => {
 
   return (
     <SafeView>
-      <TempButtonContainer>
-        <TempButton
-          title="temp New NPC"
+      <ButtonsContainer>
+        <HeaderButton
+          // title="temp New NPC"
           onPress={() => {
             navigation.navigate("Create NPC");
           }}
-        />
-      </TempButtonContainer>
+        >
+          <ButtonText>Create New NPC</ButtonText>
+        </HeaderButton>
+        <HeaderButton>
+          <ButtonText>Search will go here</ButtonText>
+        </HeaderButton>
+      </ButtonsContainer>
       <NPCView>
         {campaign && (
           <>
